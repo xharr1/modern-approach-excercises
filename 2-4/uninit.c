e4dc91821c71e577b806b0ac5609012edd8fea70
xharr1 <harrison.plummer@gmail.com>
2024-06-15 11:24:34 -0600

    created a program to see what happens with uninitialized variables

---
+++ 2-4/uninit.c
@@ -0,0 +1,16 @@
+// Test to see what happens with uninitialized ints and floats
+#include <stdio.h>
+
+int main(void) {
+  int a, b, c, d;
+  float e, f, g, h;
+
+  printf("%d\n", a);
+  printf("%d\n", b);
+  printf("%d\n", c);
+  printf("%d\n", d);
+  printf("%f\n", e);
+  printf("%f\n", f);
+  printf("%f\n", g);
+  printf("%f\n", h);
+}
