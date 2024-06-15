54da9f49a49ab29970fa578eb3226d9aa88cd23e
xharr1 <harrison.plummer@gmail.com>
2024-06-15 11:10:59 -0600

    consolidated code

--- 2-4/dweight.c
+++ 2-4/dweight.c
@@ -4,17 +4,14 @@
 
 int main(void)
 {
-	int height, length, width, volume, weight;
+	int height = 8, length = 10, width = 12, volume, weight;
 
-	height = 8;
-	length = 10;
-	width = 12;
 	volume = height * length * width;
 	weight = (volume + 165) / 166;
 
-	printf("Dimensions: %dx%dx%dx\n", length, width, height);
+	printf("Dimensions: %dx%dx%d\n", length, width, height);
 	printf("Volume (cubic inches): %d\n", volume);
-	printf("Dimensional weight (pounds): %d\n", weight);
+	printf("Dimensional weight (pounds): %d\n", ((volume + 165) / 166));
 
 	return 0;
 }
